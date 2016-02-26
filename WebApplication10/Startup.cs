2e327aa1e29ea3926b025df759a7207b20775fc2
王思閎 <wangszuhung1@outlook.com>
2016-02-27 00:13:08 +0800

    MSPTST

---
+++ WebApplication10/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication10.Startup))]
+namespace WebApplication10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
